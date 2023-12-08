f6a3b6516d48ac145b68651df15afb047c113b9b
Dhananjay-Chauhan <dj.0.omicron.mic@gmail.com>
2023-12-08 21:31:48 +0530

    removed server

--- server/routes/report.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { poseReport,exerciseReport } from "../controllers/report.js";
-
-const router = express.Router();
-
-router.post("/poseReport", poseReport);
-router.post("/exerciseReport", exerciseReport);
-
-export default router;
